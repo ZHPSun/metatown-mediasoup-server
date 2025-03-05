64550238d2e00429906e80968ba5f42880aed127
Long Zhao <long.zhao@qantashotels.com>
2025-03-05 21:12:12 +1100

    update start_server.sh to echo public IP and set ANNOUNCED_IP for pm2 startup

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,7 +7,6 @@ cd /home/ec2-user/app
 
 TOKEN=$(curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 PUBLIC_IP=$(curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+echo "Public IP: $PUBLIC_IP"
 
-echo "ANNOUNCED_IP=$PUBLIC_IP" > /home/ec2-user/app/.env
-
-pm2 start index.js --env-file .env
+ANNOUNCED_IP=$PUBLIC_IP pm2 start index.js --env-file .env
