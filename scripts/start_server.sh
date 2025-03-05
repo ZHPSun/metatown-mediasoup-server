376c2daaead96b9966907ca3a6353ec20c7004b6
Long Zhao <long.zhao@qantashotels.com>
2025-03-05 21:03:18 +1100

    add public IP retrieval and environment file setup in start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,4 +5,9 @@ echo "ApplicationStart: Starting server"
 
 cd /home/ec2-user/app
 
-pm2 start index.js
+TOKEN=$(curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+PUBLIC_IP=$(curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+
+echo "ANNOUNCED_IP=$PUBLIC_IP" > /home/ec2-user/app/.env
+
+pm2 start index.js --env-file /home/ec2-user/app/.env
