0c65255c8c2f5a327f9d670ef1deaa2a7a0c5f0b
Long Zhao <long.zhao@qantashotels.com>
2025-03-05 21:13:41 +1100

    remove environment file reference from pm2 startup in start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -9,4 +9,4 @@ TOKEN=$(curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-met
 PUBLIC_IP=$(curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 echo "Public IP: $PUBLIC_IP"
 
-ANNOUNCED_IP=$PUBLIC_IP pm2 start index.js --env-file .env
+ANNOUNCED_IP=$PUBLIC_IP pm2 start index.js
