21b80ead7f81645dbe7d7805076d0fbe7d1f80fe
Long Zhao <long.zhao@qantashotels.com>
2025-03-07 22:11:58 +1100

    fix permissions for Let's Encrypt directories in after_install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,6 +6,9 @@ echo "AfterInstall: Fixing permissions and installing dependencies"
 sudo chown -R ec2-user:ec2-user /home/ec2-user/app
 sudo chmod -R 755 /home/ec2-user/app
 
+sudo chmod -R 755 /etc/letsencrypt/live
+sudo chmod -R 755 /etc/letsencrypt/archive
+
 cd /home/ec2-user/app
 
 npm install
